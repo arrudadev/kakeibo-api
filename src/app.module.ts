bc51aaca26b4e689b1ec6cd953d6993223175f77
Alexandre Arruda <alexandre.monteiro.bec@gmail.com>
2025-05-19 14:21:19 -0300

    setup initial structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+
+import { CategoriesModule } from '@/modules/categories/categories.module'
+import { MerchantsModule } from '@/modules/merchants/merchants.module'
+import { TransactionsModule } from '@/modules/transactions/transactions.module'
+import { UsersModule } from '@/modules/users/users.module'
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UsersModule, TransactionsModule, MerchantsModule, CategoriesModule],
 })
 export class AppModule {}
